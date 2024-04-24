15f7921aa68b0a471003ede1b67bbc73ba08c6f3
Cihan Özdemir <109072308+cihanozdemir01@users.noreply.github.com>
2024-04-24 19:37:48 +0300

    Update KdvTutariHesapla.java

--- src/Week1/Pratik/KdvTutariHesapla.java
+++ src/Week1/Pratik/KdvTutariHesapla.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 /* Kullanıcıdan alınan para değerinin KDV'li fiyatını ve KDV tutarını hesaplayıp ekrana bastıran program.
  Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18, tutar 1000 TL'den büyük ise KDV oranı %8 olarak
  KDV tutarı hesaplanır. */
+
 public class KdvTutariHesapla {
     public static void main(String[] args) {
 
