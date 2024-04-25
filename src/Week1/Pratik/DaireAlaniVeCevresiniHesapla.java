786af1371c4f8db53451fa28f5153512dd128685
Cihan Özdemir <109072308+cihanozdemir01@users.noreply.github.com>
2024-04-25 09:59:35 +0300

    Update DaireAlaniVeCevresiniHesapla.java

--- src/Week1/Pratik/DaireAlaniVeCevresiniHesapla.java
+++ src/Week1/Pratik/DaireAlaniVeCevresiniHesapla.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 
 /* Kullanıcıdan alınan yarı çap ve merkez açı ölçüsü verisi ile daire alanı, çevresi
 ve daire parçasının alanını hesaplayan program. */
-
 public class DaireAlaniVeCevresiniHesapla {
     public static void main(String[] args) {
 
@@ -12,24 +11,21 @@ public class DaireAlaniVeCevresiniHesapla {
         double r,daireAlani,daireCevresi;
         final double pi = 3.14;
 
-// Kullanıcıdan yarı çap verisi alınır.
-
+        // Kullanıcıdan yarı çap verisi alınır.
         System.out.print("Lütfen dairenin yarıçapını giriniz: ");
         r = input.nextDouble();
         System.out.print("Lütfen merkez açı ölçüsünü giriniz: ");
         a = input.nextInt();
 
 
-// Daire alanı ve çevresi hesaplattırılır.
-
+        // Daire alanı ve çevresi hesaplattırılır.
         daireAlani = pi * Math.pow(r,2);
         daireCevresi = 2 * pi * r;
 
         System.out.println("Dairenin Alanı = " + daireAlani);
         System.out.println("Dairenin Çevresi = " + daireCevresi);
 
-//  Daire parçasının alanı hesaplanır.
-
+        //  Daire parçasının alanı hesaplanır.
         double daireParcaAlani = ( pi * (r * r) * a)/360;
 
         System.out.print("Daire parçasının alanı = " + daireParcaAlani);
