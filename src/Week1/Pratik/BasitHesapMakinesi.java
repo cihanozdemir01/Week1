4871f73b867b0ec910e453c68a59f1a8ca85d9e6
Cihan Özdemir <ozdemircihan@gmail.com>
2024-04-26 16:18:21 +0300

    Uçak bilet fiyatı hesaplama programı yüklendi.

--- src/Week1/Pratik/BasitHesapMakinesi.java
+++ src/Week1/Pratik/BasitHesapMakinesi.java
@@ -5,16 +5,19 @@ public class BasitHesapMakinesi {
     public static void main(String[] args) {
         int n1,n2,select;
 
+        //Kullanıcıdan 2 sayı alınır.
         Scanner input= new Scanner(System.in);
         System.out.print("İlk sayıyı giriniz: ");
         n1=input.nextInt();
         System.out.print("İkinci sayıyı giriniz: ");
         n2=input.nextInt();
 
+        //Kullanıcıdan yapmak istediği işlem alınır.
         System.out.println("Yapmak istediğiniz işlemi seçiniz.");
         System.out.println("1-Toplama\n2-Çıkarma\n3-Bölme\n4-Çarpma");
         select=input.nextInt();
 
+        //Alınan işlem talebine göre toplama,çıkarma,bölme ya da çarpma işlemi gerçekleştirilir.
         switch (select){
             case 1:
                 System.out.println("Toplama işleminizin sonucu");
@@ -38,8 +41,5 @@ public class BasitHesapMakinesi {
                 System.out.print(n1 + " x " + n2 + " = " + (n1*n2));
                 break;
         }
-
-
-
     }
 }
