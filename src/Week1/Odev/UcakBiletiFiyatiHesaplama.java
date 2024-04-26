98ea8c17c02631fc60832ffb745231cf8d8c6319
Cihan Özdemir <ozdemircihan@gmail.com>
2024-04-26 11:56:50 +0300

    Uçak bilet fiyatı hesaplama programı yüklendi.

--- src/Week1/Odev/UcakBiletiFiyatiHesaplama.java
+++ src/Week1/Odev/UcakBiletiFiyatiHesaplama.java
@@ -8,11 +8,12 @@ public class UcakBiletiFiyatiHesaplama {
         //Değişkenler tanımlanır, mesafe ücretine ve indirim oranlarına sabit bir değer atanır.
         Scanner input = new Scanner(System.in);
         double mesafeUcreti = 0.1;
-        double yasIndirimi = 0, yolculukTipiIndirimi = 0;
         double yasIndirimOrani12 = 0.5;
         double yasIndirimOrani1224 = 0.1;
         double yasIndirimOrani65 = 0.3;
         double yolculukTipiIndirimOrani = 0.2;
+        double yasIndirimi=0;
+        double yolculukTipiIndirimi=0;
 
         //Kullanıcıdan uçuş mesafesi, yaşı ve yolculuk tipi verisi alınır.
         System.out.print("Mesafeyi km türünden giriniz : ");
@@ -25,18 +26,21 @@ public class UcakBiletiFiyatiHesaplama {
         //Uçuş mesafesi ve yolcu yaşının pozitif olması ile yolculuk tipinde 2 alternatiften birinin gerçekleşmesi durumu koşula bağlanır.
         if ((ucusMesafesi > 0 && yolcuYasi > 0) && (yolculukTipi == 1 || yolculukTipi == 2)) {
 
-            ///////////////////////////////             SENARYO 1                //////////////////////////////////
-            //Yaş indiriminde yolcuya "<12" ise ücretin %50 oranında, "12< <24" ise ücretin %90 oranında, ">65" ise ücretin %70 oranında indirim uygulanacak.
-            //Yolculuk tipi indirimi tek yön ise indirim yok. Gidiş dönüş ise %80 oranında indirim olacak. Ancak 2 bilet gibi düşünmek gerekiyor toplam tutarı bulurken.
-            yasIndirimi = yolcuYasi < 12 ? (1 - yasIndirimOrani12) : yolcuYasi < 24 ? (1 - yasIndirimOrani1224) : yolcuYasi > 65 ? (1 - yasIndirimOrani65) : 1;
+            /* -Yaş indiriminde yolcuya "_<12" ise ücretin %50 oranında, "12<_<24" ise ücretin %90 oranında,
+                "_>65" ise ücretin %70 oranında indirim uygulanacak.
+               -Yolculuk tipi indirimi tek yön ise indirim yok. Gidiş dönüş ise %80 oranında indirim olacak.
+                Ancak 2 bilet gibi düşünmek gerekiyor toplam tutarı bulurken. */
+            yasIndirimi = yolcuYasi < 12 ? (1 - yasIndirimOrani12) : yolcuYasi < 24 ?
+                        (1 - yasIndirimOrani1224) : yolcuYasi > 65 ? (1 - yasIndirimOrani65) : 1;
             yolculukTipiIndirimi = yolculukTipi == 1 ? 1 : 2 * (1 - yolculukTipiIndirimOrani);
+
         } else {
             System.out.println("Hatalı Veri Girdiniz !");
             System.exit(0);
         }
 
         double biletFiyati = mesafeUcreti * ucusMesafesi * yasIndirimi * yolculukTipiIndirimi;
-        System.out.println("Toplam Tutar =" + biletFiyati + " TL");
+        System.out.println("Toplam Tutar = " + biletFiyati + " TL");
 
     }
 }
