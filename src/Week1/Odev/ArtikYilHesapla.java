ee85b1ac866f934b11e5a474786db95df8d756bc
Cihan Özdemir <ozdemircihan@gmail.com>
2024-04-27 20:48:13 +0300

    Çin Zodyağı hesaplayan program yüklendi.

---
+++ src/Week1/Odev/ArtikYilHesapla.java
@@ -0,0 +1,24 @@
+package Week1.Odev;
+import java.util.Scanner;
+
+// Kullanıcıdan alınan yıl verisine göre artık yıl sorgusunu yapan program.
+public class ArtikYilHesapla {
+    public static void main(String[] args) {
+
+        int year;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Yıl Giriniz : ");
+        year = input.nextInt();
+
+        // Sayı 400'e tam bölünebiliyorsa artık yıldır. 4'e tam bölünüp 100'e tam bölünemiyorsa da artık yıldır.,
+        // Aksi durumda artık yıl değildir.
+        if ( year % 400 == 0) {
+            System.out.println( year + " Artık bir yıldır !");
+        } else if ( year % 4 == 0 && year % 100 !=0) {
+            System.out.println(year + " Artık bir yıldır !");
+        } else {
+            System.out.println("Artık bir yıl değildir !");
+        }
+    }
+}
