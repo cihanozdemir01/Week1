b048f634d8b2c6311e44450c7266f58b5d89404b
Cihan Özdemir <ozdemircihan@gmail.com>
2024-04-27 20:26:39 +0300

    Çin Zodyağı hesaplayan program yüklendi.

--- src/Week1/Odev/CinZodyagiHesapla.java
+++ src/Week1/Odev/CinZodyagiHesapla.java
@@ -17,40 +17,40 @@ public class CinZodyagiHesapla {
 
         switch(cond){
             case 0:
-                System.out.print("Maymun");
+                System.out.print("Çin Zodyağı Burcunuz : Maymun");
                 break;
             case 1:
-                System.out.print("Horoz");
+                System.out.print("Çin Zodyağı Burcunuz : Horoz");
                 break;
             case 2:
-                System.out.print("Köpek");
+                System.out.print("Çin Zodyağı Burcunuz : Köpek");
                 break;
             case 3:
-                System.out.print("Domuz");
+                System.out.print("Çin Zodyağı Burcunuz : Domuz");
                 break;
             case 4:
-                System.out.print("Fare");
+                System.out.print("Çin Zodyağı Burcunuz : Fare");
                 break;
             case 5:
-                System.out.print("Öküz");
+                System.out.print("Çin Zodyağı Burcunuz : Öküz");
                 break;
             case 6:
-                System.out.print("Kaplan");
+                System.out.print("Çin Zodyağı Burcunuz : Kaplan");
                 break;
             case 7:
-                System.out.print("Tavşan");
+                System.out.print("Çin Zodyağı Burcunuz : Tavşan");
                 break;
             case 8:
-                System.out.print("Ejderha");
+                System.out.print("Çin Zodyağı Burcunuz : Ejderha");
                 break;
             case 9:
-                System.out.print("Yılan");
+                System.out.print("Çin Zodyağı Burcunuz : Yılan");
                 break;
             case 10:
-                System.out.print("At");
+                System.out.print("Çin Zodyağı Burcunuz : At");
                 break;
             case 11:
-                System.out.print("Koyun");
+                System.out.print("Çin Zodyağı Burcunuz : Koyun");
                 break;
         }
     }
