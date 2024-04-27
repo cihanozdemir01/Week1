1e1ee4b5d26c7d8a5b1606d5944c29fc8af39da4
Cihan Özdemir <ozdemircihan@gmail.com>
2024-04-27 21:03:12 +0300

    Artık yıl hesabı yapan program yüklendi.

--- src/Week1/Odev/CinZodyagiHesapla.java
+++ src/Week1/Odev/CinZodyagiHesapla.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CinZodyagiHesapla {
     public static void main(String[] args) {
 
-        int birthDate, cond;
+        int birthDate;
 
         // Kullanıcıdan veri alınır.
         Scanner input = new Scanner(System.in);
@@ -13,46 +13,22 @@ public class CinZodyagiHesapla {
         birthDate = input.nextInt();
 
         // Çin Zodyağı hesabı için doğum tarihinin 12'ye bölümünden kalan bir değişkene atanır.
-        cond = birthDate % 12;
-
-        switch(cond){
-            case 0:
-                System.out.print("Çin Zodyağı Burcunuz : Maymun");
-                break;
-            case 1:
-                System.out.print("Çin Zodyağı Burcunuz : Horoz");
-                break;
-            case 2:
-                System.out.print("Çin Zodyağı Burcunuz : Köpek");
-                break;
-            case 3:
-                System.out.print("Çin Zodyağı Burcunuz : Domuz");
-                break;
-            case 4:
-                System.out.print("Çin Zodyağı Burcunuz : Fare");
-                break;
-            case 5:
-                System.out.print("Çin Zodyağı Burcunuz : Öküz");
-                break;
-            case 6:
-                System.out.print("Çin Zodyağı Burcunuz : Kaplan");
-                break;
-            case 7:
-                System.out.print("Çin Zodyağı Burcunuz : Tavşan");
-                break;
-            case 8:
-                System.out.print("Çin Zodyağı Burcunuz : Ejderha");
-                break;
-            case 9:
-                System.out.print("Çin Zodyağı Burcunuz : Yılan");
-                break;
-            case 10:
-                System.out.print("Çin Zodyağı Burcunuz : At");
-                break;
-            case 11:
-                System.out.print("Çin Zodyağı Burcunuz : Koyun");
-                break;
-        }
+        String burc = switch (birthDate % 12) {
+            case 0 -> "Maymun";
+            case 1 -> "Horoz";
+            case 2 -> "Köpek";
+            case 3 -> "Domuz";
+            case 4 -> "Fare";
+            case 5 -> "Öküz";
+            case 6 -> "Kaplan";
+            case 7 -> "Tavşan";
+            case 8 -> "Ejderha";
+            case 9 -> "Yılan";
+            case 10 -> "At";
+            case 11 -> "Koyun";
+            default -> "";
+        };
+        System.out.println("Çin Zodyağı Burcunuz : " + burc);
     }
 }
 
