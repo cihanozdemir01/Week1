4991e8d53411126a63b32340be9fa7d46306d711
Cihan Özdemir <ozdemircihan@gmail.com>
2024-04-30 00:21:00 +0300

    Ters üçgen oluşturan program yüklendi.

---
+++ src/Week1/Odev/TersUcgen.java
@@ -0,0 +1,22 @@
+package Week1.Odev;
+import java.util.Scanner;
+
+// Kullanıcıdan alınan sayıya göre ters üçgen oluşturan program.
+public class TersUcgen {
+    public static void main(String[] args) {
+
+        int num;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz : ");
+        num = inp.nextInt();
+
+        /* Ters üçgen oluştururken bir bağıntı bulmak gerekiyor. Burada da "2*(num-i)-1"
+           bağıntısı bulunarak işlem gerçekleştirilmiştir. */
+        for (int i=1; i<=num; i++) {
+            for (int y=1; y<=2*(num-i)-1;y++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
