09c102e29a1a80b12b876cf9d0964fe7987601b4
Cihan Özdemir <ozdemircihan@gmail.com>
2024-05-01 19:39:42 +0300

    Recursive metot ile üs alma programı yüklendi.

--- src/Week2/AsalSayiBul.java
+++ src/Week2/AsalSayiBul.java
@@ -3,29 +3,24 @@ import java.util.Scanner;
 
 // Girilen sayının asal olup/olmadığını bulan program.
 public class AsalSayiBul {
-    static String prime(int a){
 
-        boolean check=false;
-
-        for(int i=2; i<a; i++){
-            if( a % i == 0) {
-                check=true;
-                break;
-            }
-        }
-        if (check){
-            return "Asal sayı değil";
-        }else{
-            return "Asal sayı";
-        }
+    static boolean isPrime(int n, int divisor) {
+        if (n < 2)
+            return false;
+        if (divisor == 1)
+            return true;
+        if (n % divisor == 0)
+            return false;
+        return isPrime(n, divisor - 1);
     }
     public static void main(String[] args) {
-
         System.out.print("Sayı giriniz : ");
         Scanner scan = new Scanner(System.in);
         int num = scan.nextInt();
 
-        System.out.println(prime(num));
-
+        if (isPrime(num, num / 2))
+            System.out.println(num + " bir asal sayıdır.");
+        else
+            System.out.println(num + " bir asal sayı değildir.");
     }
 }
