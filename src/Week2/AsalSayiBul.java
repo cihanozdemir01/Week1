e40fce4812db1539bfc7aaaa0b001a9e4fd7d565
Cihan Özdemir <ozdemircihan@gmail.com>
2024-05-01 13:34:01 +0300

    Asal sayıları bulan program güncellendi.

---
+++ src/Week2/AsalSayiBul.java
@@ -0,0 +1,30 @@
+package Week2;
+import java.util.Scanner;
+
+public class AsalSayiBul {
+    static String prime(int a){
+
+        boolean check=false;
+
+        for(int i=2; i<a; i++){
+            if( a % i == 0) {
+                check=true;
+                break;
+            }
+        }
+        if (check){
+            return "Asal sayı değil";
+        }else{
+            return "Asal sayı";
+        }
+    }
+    public static void main(String[] args) {
+
+        System.out.print("Sayı giriniz : ");
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+
+        System.out.println(prime(num));
+
+    }
+}
