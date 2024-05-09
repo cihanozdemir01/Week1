007c9e2daaa8ffeb7ac63c10ec8951d2bdfea3c4
Cihan Özdemir <ozdemircihan@gmail.com>
2024-05-09 10:20:53 +0300

    Ekrana harf yazdıran program yüklendi.

---
+++ src/Week2/NearestNumber.java
@@ -0,0 +1,33 @@
+package Week2;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class NearestNumber {
+    public static void main(String[] args) {
+        int[] list = {15, 12, 788, 1, -1, -778, 2, 0};
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Girilen Sayı: ");
+        int number = scanner.nextInt();
+
+        Arrays.sort(list);
+
+        int nearestSmaller = Integer.MIN_VALUE;
+        int nearestLarger = Integer.MAX_VALUE;
+
+        for (int i : list) {
+            if (i < number && i > nearestSmaller) {
+                nearestSmaller = i;
+            }
+            if (i > number && i < nearestLarger) {
+                nearestLarger = i;
+            }
+        }
+
+        // Sonuçları yazdır
+        System.out.println("Girilen sayıdan küçük en yakın sayı: " + nearestSmaller);
+        System.out.println("Girilen sayıdan büyük en yakın sayı: " + nearestLarger);
+    }
+}
+        
+
